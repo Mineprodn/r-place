7214ca818f4e11d8883d719e221d2869054dcdfe
Mineprodn <proxima3141@gmail.com>
2025-06-07 13:19:02 +0200

    script.js

--- public/script.js
+++ public/script.js
@@ -1,41 +1,50 @@
-let allPixels = {}; // global speichern
+const express = require("express");
+const app = express();
+const http = require("http").createServer(app);
+const io = require("socket.io")(http);
+const fs = require("fs");
 
-socket.on('init', ({ pixels, gridWidth: w, gridHeight: h }) => {
-  gridWidth = w;
-  gridHeight = h;
-  allPixels = pixels;
-  widthInput.value = w;
-  heightInput.value = h;
-  resizeCanvas();
-  drawAllPixels();
+let pixels = {};
+let gridWidth = 100;
+let gridHeight = 100;
+
+try {
+  const saved = JSON.parse(fs.readFileSync("canvas_data.json"));
+  pixels = saved.pixels || {};
+  gridWidth = saved.width || 100;
+  gridHeight = saved.height || 100;
+} catch (e) {}
+
+app.use(express.static("public"));
+
+io.on("connection", (socket) => {
+  io.emit("user_count", io.engine.clientsCount);
+  socket.emit("init", { pixels, gridWidth, gridHeight });
+
+  socket.on("place_pixel", ({ x, y, color }) => {
+    pixels[`${x},${y}`] = color;
+    io.emit("update_pixel", { x, y, color });
+    save();
+  });
+
+  socket.on("resize_canvas", ({ width, height }) => {
+    gridWidth = width;
+    gridHeight = height;
+    io.emit("canvas_resized", { width, height });
+    save();
+  });
+
+  socket.on("disconnect", () => {
+    io.emit("user_count", io.engine.clientsCount);
+  });
 });
 
-socket.on('canvas_resized', ({ width, height }) => {
-  gridWidth = width;
-  gridHeight = height;
-  widthInput.value = width;
-  heightInput.value = height;
-  resizeCanvas();
-  drawAllPixels();
-});
-
-socket.on('update_pixel', ({ x, y, color }) => {
-  const key = `${x},${y}`;
-  allPixels[key] = color;
-  drawPixel(x, y, color);
-});
-
-function resizeCanvas() {
-  canvas.width = gridWidth * pixelSize;
-  canvas.height = gridHeight * pixelSize;
-  ctx.clearRect(0, 0, canvas.width, canvas.height);
+function save() {
+  fs.writeFileSync("canvas_data.json", JSON.stringify({
+    pixels, width: gridWidth, height: gridHeight
+  }));
 }
 
-function drawAllPixels() {
-  for (const key in allPixels) {
-    const [x, y] = key.split(',').map(Number);
-    if (x < gridWidth && y < gridHeight) {
-      drawPixel(x, y, allPixels[key]);
-    }
-  }
-}
+http.listen(3000, () => {
+  console.log("Server läuft auf http://localhost:3000");
+});
