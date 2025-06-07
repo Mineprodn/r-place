96a65cc798c2b913165984c94b653055be2ac65b
Mineprodn <proxima3141@gmail.com>
2025-06-07 09:15:07 +0200

    server.js

--- server.js
+++ server.js
@@ -1,23 +1,31 @@
-// ... dein bestehender Code oben
-
-let connectedUsers = 0;
+let pixels = {};
+let gridWidth = 100;   // Standardgröße
+let gridHeight = 100;
 
 io.on('connection', (socket) => {
-  connectedUsers++;
-  io.emit('user_count', connectedUsers);
-  console.log(`Client verbunden - Nutzer: ${connectedUsers}`);
+  // Neue Verbindung
+  socket.emit('init_canvas', { width: gridWidth, height: gridHeight, pixels });
 
-  socket.emit('load_pixels', pixels);
-
+  // Pixel setzen
   socket.on('place_pixel', ({ x, y, color }) => {
     const key = `${x},${y}`;
     pixels[key] = color;
     io.emit('update_pixel', { x, y, color });
   });
 
+  // Neue Größe von einem Client
+  socket.on('resize_canvas', ({ width, height }) => {
+    gridWidth = width;
+    gridHeight = height;
+    io.emit('canvas_resized', { width, height });
+  });
+
+  // Nutzerzähler
+  connectedUsers++;
+  io.emit('user_count', connectedUsers);
+
   socket.on('disconnect', () => {
     connectedUsers--;
     io.emit('user_count', connectedUsers);
-    console.log(`Client getrennt - Nutzer: ${connectedUsers}`);
   });
 });
